7d60d916002b71a075353c953ed70b1a35d8dcea
mohit77u <mohiit77u@gmail.com>
2023-02-28 17:25:51 +0530

    updated final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router';
 import React, { useState } from 'react'
 
 export default function Header() {
 
+  const router = useRouter();
   const [mobileMenu, setMobileMenu] = useState(false)
 
   return (
@@ -50,25 +52,25 @@ export default function Header() {
             {/* main menu */}
             <ul className="flex flex-col p-4 mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
               <li>
-                <Link href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:p-0 dark:text-white" aria-current="page">Home</Link>
+                <Link href="/" className="block py-2 pl-3 pr-4 text-white md:bg-transparent md:p-0">Home</Link>
               </li>
               <li>
-                <a href="#" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">What’s on</a>
+                <a href="#" className="block py-2 pl-3 pr-4 text-white md:bg-transparent md:p-0">What’s on</a>
               </li>
               <li>
-                <Link href="/book-venue" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Book Venue</Link>
+                <Link href="/book-venue" className="block py-2 pl-3 pr-4 text-white md:bg-transparent md:p-0">Book Venue</Link>
               </li>
               <li>
-                <a href="#" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Menu</a>
+                <a href="#" className="block py-2 pl-3 pr-4 text-white md:bg-transparent md:p-0">Menu</a>
               </li>
               <li>
-              <Link href="/gallery" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Gallery</Link>
+              <Link href="/gallery" className="block py-2 pl-3 pr-4 text-white md:bg-transparent md:p-0">Gallery</Link>
               </li>
               <li>
-                <a href="#" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About Us</a>
+                <a href="#" className="block py-2 pl-3 pr-4 text-white md:bg-transparent md:p-0">About Us</a>
               </li>
               <li>
-                <a href="#" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact Us</a>
+                <a href="#" className="block py-2 pl-3 pr-4 text-white md:bg-transparent md:p-0">Contact Us</a>
               </li>
             </ul>
           </div>
